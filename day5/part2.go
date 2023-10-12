ee4bc35b0c72e21379fb9fbcf97bc3d20b4e1ab1
Andriy Pashynnyk <tobirawork@gmail.com>
2023-10-12 15:15:06 +0300

    day5 part2

--- day5/part2.go
+++ day5/part2.go
@@ -8,31 +8,6 @@ import (
 	"strings"
 )
 
-type CargoStack struct {
-	stack []byte
-}
-
-func (cs *CargoStack) empty() bool {
-	return len(cs.stack) == 0
-}
-
-func (cs *CargoStack) push(cargo byte) {
-	cs.stack = append(cs.stack, cargo)
-}
-
-func (cs *CargoStack) pop() byte {
-
-	if cs.empty() {
-		panic("empty stack")
-	}
-
-	last := cs.stack[len(cs.stack)-1]
-
-	cs.stack = cs.stack[:len(cs.stack)-1]
-
-	return last
-}
-
 func main() {
 	content, err := os.ReadFile("input.txt")
 	if err != nil {
@@ -43,14 +18,14 @@ func main() {
 	staksStrings := strings.Split(splitted[0], "\n")
 	staksStrings = staksStrings[:len(staksStrings)-1] // removing last numerical row
 
-	stacks := make([]CargoStack, 9)
+	stacks := make([][]byte, 9)
 
 	for j := len(staksStrings) - 1; j >= 0; j -= 1 {
 		for i := 0; i < 9; i += 1 {
 			cargo := staksStrings[j][i*4+1]
 
 			if 'A' <= cargo && cargo <= 'Z' {
-				stacks[i].push(cargo)
+				stacks[i] = append(stacks[i], cargo)
 			}
 		}
 	}
@@ -69,9 +44,12 @@ func main() {
 		from -= 1 // adjust to indeces
 		to -= 1   // adjust to indeces
 
-		for k := 0; k < amount; k += 1 {
-			stacks[to].push(stacks[from].pop())
-		}
+		l := len(stacks[from]) - amount
+
+		topCargos := stacks[from][l:]
+
+		stacks[to] = append(stacks[to], topCargos...)
+		stacks[from] = stacks[from][:l]
 	}
 
 	// fmt.Println(stacks)
@@ -79,7 +57,7 @@ func main() {
 	top := make([]byte, 9)
 
 	for _, stack := range stacks {
-		top = append(top, stack.pop())
+		top = append(top, stack[len(stack)-1])
 	}
 
 	fmt.Println(string(top))
